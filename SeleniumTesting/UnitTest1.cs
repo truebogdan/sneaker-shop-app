d13bfa22004c8e40c16d1c5655f9c1115a231e61
Florin Niculae <florin_nic07@yahoo.com>
2022-12-07 17:48:57 +0200

    Added Selenium Test Project

---
+++ SeleniumTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SeleniumTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
