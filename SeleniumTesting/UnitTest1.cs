158dae351fd3e5d1eafa75dbde4645beb5394e0b
Florin Niculae <florin_nic07@yahoo.com>
2022-12-13 09:21:29 +0200

    SeleniumTest - Verify landing page

--- SeleniumTesting/UnitTest1.cs
+++ SeleniumTesting/UnitTest1.cs
@@ -1,16 +1,49 @@
+using NUnit.Framework;
+
+using OpenQA.Selenium;
+
+using OpenQA.Selenium.Chrome;
+
+using OpenQA.Selenium.Firefox;
+
+using System;
+
+using System.Collections.ObjectModel;
+
+using System.IO;
+
 namespace SeleniumTesting
 {
     public class Tests
     {
-        [SetUp]
+
+        IWebDriver driver;
+
+        [OneTimeSetUp]
         public void Setup()
         {
+            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+            driver = new ChromeDriver(path + @"\drivers\");
+
         }
 
         [Test]
-        public void Test1()
+
+        public void verifyInitialPage()
         {
-            Assert.Pass();
+            driver.Navigate().GoToUrl("http://localhost:7249");  //open the application
+
+            //verify nav bar buttons
+            Assert.IsTrue(driver.FindElement(By.XPath("//a[contains(text(), 'Register')]")).Displayed);
+            Assert.IsTrue(driver.FindElement(By.XPath("//a[contains(text(), 'Login')]")).Displayed);
+            Assert.IsTrue(driver.FindElement(By.XPath("//a[contains(text(), 'Home')]")).Displayed);
+            Assert.IsTrue(driver.FindElement(By.XPath("//a[contains(text(), 'Shop')]")).Displayed);
+
+            //verify middle h1 text
+            Assert.IsTrue(driver.FindElement(By.XPath("h1[contains(text(), 'Welcome to SneakerShop!')]")).Displayed);
+            
+
         }
     }
 }
